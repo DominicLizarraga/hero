4d5ecc03bc617176933b66361f9e82111aa18abf
dominic lizarraga <dominiclizarraga@hotmail.com>
2021-10-09 22:45:04 -0700

    bootstrap installed

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,10 +3,15 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
+import "bootstrap"
+import './src/application.scss'
+
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+
+require("bootstrap/dist/js/bootstrap")
 
 Rails.start()
 Turbolinks.start()
