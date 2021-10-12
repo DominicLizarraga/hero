eb6f9a9f119fb4a73d6ae05663622b2a27f7add7
dominic lizarraga <dominiclizarraga@hotmail.com>
2021-10-11 19:35:20 -0700

    added random avatar image

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,6 +6,11 @@ class HomeController < ApplicationController
     @hero_power = Faker::Superhero.power
     @corp_power = Faker::Company.bs
     @corp_title = Faker::Company.profession
+
+    @rand_num = rand(1..1000)
+    @set_number = rand(1..4)
+
+    @avatar = Faker::Avatar.image(slug: @rand_num, size: "200x200", format: "png", set: "set#{@set_number}", bgset: "bg1")
   end
 
   def index
