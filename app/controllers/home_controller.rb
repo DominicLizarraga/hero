1796b5b77b6e3fcc220c6235df155855732e63fe
dominic lizarraga <dominiclizarraga@hotmail.com>
2021-10-11 19:06:05 -0700

    added forms and connected the gem

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,18 @@
 class HomeController < ApplicationController
-  def index
+  before_action :common_content, only: [:index, :team]
+
+  def common_content
+    @hero_name = Faker::Superhero.name
+    @hero_power = Faker::Superhero.power
+    @corp_power = Faker::Company.bs
+    @corp_title = Faker::Company.profession
   end
+
+  def index
+
+  end
+
+
   def team
 
   end
